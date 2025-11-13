26a22b7cfd6a61bbaac7fdea24227293f77f1dad
Chuz06 <chuz2923@gmail.com>
2025-11-12 19:48:32 -0600

    espacio

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,10 +7,12 @@ namespace Gestor_Proyectos_Academicos.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+
+            // holaaaa
 
         }
 
