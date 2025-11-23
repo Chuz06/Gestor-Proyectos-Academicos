7dbc2c9171bfaaa2c68bc12b62335995dec77942
Lquiroz19 <peperozqui11@gmail.com>
2025-11-23 02:54:59 -0600

    Conexion a base de datos
    Agregada prueba de conexión a base de datos y conexion a bs

--- ConexionBD.cs
+++ ConexionBD.cs
@@ -1,6 +1,42 @@
-﻿namespace Gestor_Proyectos_Academicos
+﻿using Microsoft.Data.SqlClient;
+
+namespace Gestor_Proyectos_Academicos
 {
     public class ConexionBD
     {
+        private SqlConnection conexion;
+
+        
+        public ConexionBD()
+        {
+            
+            string cadena = "Server=localhost;Database=GestorProyectos; Integrated Security=True; Encrypt=False;";
+            conexion = new SqlConnection(cadena);
+        }
+
+       
+        public void Abrir()
+        {
+            if (conexion.State == System.Data.ConnectionState.Closed)
+            {
+                conexion.Open();
+            }
+        }
+
+       
+        public void Cerrar()
+        {
+            if (conexion.State == System.Data.ConnectionState.Open)
+            {
+                conexion.Close();
+            }
+        }
+
+        
+        public SqlConnection ObtenerConexion()
+        {
+            return conexion;
+        }
+
     }
 }
